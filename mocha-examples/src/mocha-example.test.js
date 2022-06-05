4acb08d9ad6d967521d5a1d102fa6b668fa90037
Lars Thorup <lars@zealake.com>
2022-06-05 23:00:52 +0200

    describe: nested, in defined order

--- mocha-examples/src/mocha-example.test.js
+++ mocha-examples/src/mocha-example.test.js
@@ -1,7 +1,18 @@
 import { strict as assert } from 'node:assert';
 
-describe('top-level', function () {
-  it('should pass', function () {
+describe('outer', () => {
+  it('outer first', () => {
+    assert.equal(2 + 2, 4);
+  });
+  describe('inner', () => {
+    it('inner first', () => {
+      assert.equal(2 + 2, 4);
+    });
+    it('inner last', () => {
+      assert.equal(2 + 2, 4);
+    });
+  });
+  it('outer last', () => {
     assert.equal(2 + 2, 4);
   });
 });
--- testrunner/packages/testrunner/index.js
+++ testrunner/packages/testrunner/index.js
@@ -1,34 +1,49 @@
-const globalDescribe = { type: 'describe', tests: [] };
-const tests = [globalDescribe];
-let currentDescribe = globalDescribe;
+const globalTest = { type: 'describe', testList: [] };
+let currentTest = globalTest;
 
 export const describe = (name, fn) => {
-  console.log('describe', name);
-  const describe = { type: 'describe', name, tests: [] };
-  currentDescribe.tests.push(describe);
-  const previousDescribe = currentDescribe;
-  currentDescribe = describe;
+  // console.log('describe', name);
+  const describe = { type: 'describe', name, testList: [] };
+  currentTest.testList.push(describe);
+  const previousTest = currentTest;
+  currentTest = describe;
   fn();
-  currentDescribe = previousDescribe;
+  currentTest = previousTest;
 };
 
 export const it = (name, fn) => {
-  console.log('it', name);
+  // console.log('it', name);
   const it = { type: 'it', name, fn };
-  currentDescribe.tests.push(it);
+  currentTest.testList.push(it);
 };
 
+// TODO: beforeAll, beforeEach, afterAll, afterEach
+
 export const runner = () => {
-  // TODO: iterate
-  const describe = globalDescribe.tests[0];
-  const it = describe.tests[0];
-  const { name, fn } = it;
-  console.log('before', describe.name, name);
-  try {
-    fn();
-    console.log('pass', describe.name, name)
-  } catch (ex) {
-    console.log('fail', describe.name, name, ex.message)
+  run(globalTest, []);
+  // TODO: stats
+}
+
+const run = (test, parentTestList) => {
+  for (const childTest of test.testList) {
+    const { type } = childTest;
+    const fullTestList = parentTestList.concat([childTest]);
+    switch (type) {
+      case 'describe':
+        run(childTest, fullTestList);
+        break;
+      case 'it':
+        const fullName = fullTestList.map(({ name }) => name).join(' - ');
+        const { fn } = childTest;
+        // console.log('before', fullName);
+        try {
+          fn();
+          console.log('✔', fullName)
+        } catch (ex) {
+          console.log('x', fullName, ex.message)
+        }
+        // console.log('after', fullName);
+        break;
+    }
   }
-  console.log('after', describe.name, name);
 }
