ba6a31a5c52d2585a1f6b9d0192cb7d44dbf3744
Lars Thorup <lars@zealake.com>
2022-06-11 19:56:27 +0200

    multiple test files

--- packages/testrunner/src/cli.js
+++ packages/testrunner/src/cli.js
@@ -3,14 +3,17 @@
 import { runner } from './index.js';
 
 // TODO: use glob arg to find test files
-import '../../examples/src/example.test.js';
-import '../../examples/src/chai-example.test.js';
+const testFilePaths = [
+  '../../examples/src/example.test.js',
+  '../../examples/src/chai-example.test.js',
+];
 
 const main = async () => {
   // TODO: use watcher to provide additional test filter
   // TODO: instrument for coverage
   // TODO: run in browser or node
   // TODO: tab / process / worker isolation or not
+  await Promise.all(testFilePaths.map((testFilePath) => import(testFilePath)));
   await runner();
   // TODO: report test results
   // TODO: report coverage
