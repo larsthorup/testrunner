cf1804244b91942fe756894218c8abf927080a92
Lars Thorup <lars@zealake.com>
2022-06-21 20:28:58 +0200

    withSetup; wip

--- packages/examples/src/withSetup-example.test.js
+++ packages/examples/src/withSetup-example.test.js
@@ -4,37 +4,33 @@ import { strict as assert } from 'node:assert';
 import { afterAll, beforeAll, describe, it } from '@larsthorup/testrunner';
 
 // withSetup - use describe to model a fixture
-const useSetup = (setup) => {
-  const { before, after, get } = setup();
-  beforeAll(before);
-  afterAll(after);
-  return get;
-};
 describe('withSetup', () => {
   let order = [];
   describe('block', () => {
-    const getDb = useSetup(() => {
-      let db;
-      return {
-        before: () => {
+    const withDb = (block) => {
+      describe('withDb', () => {
+        let db;
+        beforeAll(() => {
           db = { some: 'db' };
           order.push('setup db');
-        },
-        after: () => {
+        });
+        afterAll(() => {
           db = undefined;
           order.push('teardown db');
-        },
-        get: () => {
-          order.push('get db');
-          return db;
-        },
-      };
-    });
-    const withServer = (block) => {
+        });
+        block({
+          beforeAll: (fn) => {
+            beforeAll(() => {
+              fn({ db });
+            });
+          },
+        });
+      });
+    };
+    const withServer = (ctx, block) => {
       describe('withServer', () => {
         let server;
-        beforeAll(() => {
-          const db = getDb();
+        ctx.beforeAll(({ db }) => {
           server = { db };
           order.push('setup server');
         });
@@ -51,17 +47,18 @@ describe('withSetup', () => {
         });
       });
     };
-    withServer(({ it }) => {
-      it('should have setup', ({ server }) => {
-        assert.deepEqual(server, { db: { some: 'db' } });
-        order.push('test');
-      });
-    });
+    withDb((ctx) =>
+      withServer(ctx, (ctx) => {
+        ctx.it('should have setup', ({ server }) => {
+          assert.deepEqual(server, { db: { some: 'db' } });
+          order.push('test');
+        });
+      })
+    );
   });
   afterAll(() => {
     assert.deepEqual(order, [
       'setup db',
-      'get db',
       'setup server',
       'test',
       'teardown server',
