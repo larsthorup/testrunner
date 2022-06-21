b492900904f542106af3a69ef9ccf894eede38ef
Lars Thorup <lars@zealake.com>
2022-06-21 22:08:19 +0200

    withSetup; wip

--- packages/examples/src/withSetup-example.test.js
+++ packages/examples/src/withSetup-example.test.js
@@ -3,7 +3,8 @@
 import { strict as assert } from 'node:assert';
 import * as test from '@larsthorup/testrunner';
 
-// withSetup - use describe to model a fixture
+// withSetup - use describe to model a composable fixture
+// TODO: async
 const use = (test, get, block) => {
   block({
     ...test,
@@ -19,37 +20,40 @@ const use = (test, get, block) => {
     },
   });
 };
+const withSetup = (name, setup) => {
+  return (test, block) => {
+    test.describe(`withSetup(${name})`, () => {
+      setup({ ...test, use: (get) => use(test, get, block) });
+    });
+  };
+};
 test.describe('withSetup', () => {
   let order = [];
   test.describe('block', () => {
-    const withDb = (test, block) => {
-      test.describe('withDb', () => {
-        let db;
-        test.beforeAll(() => {
-          db = { some: 'db' };
-          order.push('setup db');
-        });
-        test.afterAll(() => {
-          db = undefined;
-          order.push('teardown db');
-        });
-        use(test, () => ({ db }), block);
+    const withDb = withSetup('db', (test) => {
+      let db;
+      test.beforeAll(() => {
+        db = { some: 'db' };
+        order.push('setup db');
       });
-    };
-    const withServer = (test, block) => {
-      test.describe('withServer', () => {
-        let server;
-        test.beforeAll(({ db }) => {
-          server = { db };
-          order.push('setup server');
-        });
-        test.afterAll(() => {
-          server = undefined;
-          order.push('teardown server');
-        });
-        use(test, () => ({ server }), block);
+      test.afterAll(() => {
+        db = undefined;
+        order.push('teardown db');
       });
-    };
+      test.use(() => ({ db }));
+    });
+    const withServer = withSetup('server', (test) => {
+      let server;
+      test.beforeAll(({ db }) => {
+        server = { db };
+        order.push('setup server');
+      });
+      test.afterAll(() => {
+        server = undefined;
+        order.push('teardown server');
+      });
+      test.use(() => ({ server }));
+    });
     const withInfra = (test, block) => {
       withDb(test, (test) => {
         withServer(test, (test) => {
